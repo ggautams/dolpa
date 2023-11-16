884691c97360e330b529c27ff3e7558270245ed6
ggautam <gpal.gtm@gmail.com>
2023-11-16 18:54:34 -0500

    Add unit test for json-like path conversion to python-like

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.dolpa.dolpa_utils import interpolate
+from src.dolpa.dolpa_utils import interpolate, get_dict_value_from_json_path
 
 
 class TestUtils(unittest.TestCase):
@@ -19,7 +19,11 @@ class TestUtils(unittest.TestCase):
             "body": {
                 "currentUser": "alex.martin",
                 "currentState": "healthy",
-                "value-1": "{{value-1}}"
+                "value-1": "{{value-1}}",
+                "sampleArray": [
+                    {"arr1": [1, 2, 3], "arr2": [4, 5, 6]},
+                    {"arr3": [7, 8, 9], "arr4": [10, 11, 12]}
+                ]
             },
             "headers": {
                 "app-status": "current"
@@ -36,3 +40,6 @@ class TestUtils(unittest.TestCase):
         interpolated = interpolate(self.test_call, self.test_config)
         self.assertEqual(interpolated['body']['value-1'], 'test-value-1')
 
+    def test_get_dict_value_from_json_path(self):
+        actual_value = get_dict_value_from_json_path(self.test_call, 'body.sampleArray[1].arr3[2]')
+        self.assertEqual(actual_value, 9)
